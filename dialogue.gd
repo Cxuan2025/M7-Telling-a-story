2a7cfa2a38395aecd3d4a6019fc753c6b9e7d170
Cxuan2025 <lmjcpqcpq@gmail.com>
2025-11-09 20:07:15 -0800

    Lesson 4

--- dialogue.gd
+++ dialogue.gd
@@ -2,19 +2,43 @@ extends Control
 
 ## An array of strings. Each string contains the text we want the character to
 ## say.
-var dialogue_items: Array[String] = [
-	"I'm hungry",
-	"But i am still working on.",
-	"When is  the lunch",
-	"Did I get it right? Did I?",
-	"Okay Bye bye~!",
-]
+var expressions := {
+	"happy": preload ("res://assets/emotion_happy.png"),
+	"regular": preload ("res://assets/emotion_regular.png"),
+	"sad": preload ("res://assets/emotion_sad.png"),
+}
+
+
+var dialogue_items: Array[Dictionary] = [
+	{
+		"expression": expressions["sad"],
+		"text": "I'm hungry",
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "But i am still working on",
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "When is  the lunch",
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Finally I can enjoy my lunch",
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Okay Bye bye~!",
+	},
+	]
 ## Holds the index of the currently displayed text
 var current_item_index := 0
 
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var expression: TextureRect = %Expression
+@onready var body: TextureRect = %Body
 
 
 func _ready() -> void:
@@ -24,7 +48,8 @@ func _ready() -> void:
 ## Draws the current text to the rich text element
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
 	# We animate the text appearing letter by letter.
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
